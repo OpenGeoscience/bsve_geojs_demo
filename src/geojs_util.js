d127a80dacc0f30273b1f4cf8666971b18763c53
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-10-16 17:04:03 -0400

    Add geojs and deps to the build

--- src/geojs_util.js
+++ src/geojs_util.js
@@ -1,15 +1,86 @@
-/* global geo */
+var geo_data_store = 'geo',
+    $ = require('jquery'),
+    geo = require('geojs');
+
+/**
+ * This object contains methods that are callable from the
+ * jquery plugin.  I.e.
+ *
+ *   $(node).geojs('data', arg1, arg2, ...) -> geo_mathods(arg1, arg2, ...);
+ */
+var geo_methods = {
+    /**
+     * Get/set data from the map instance.
+     */
+    data: function (name, data) {
+        var geoData = $(this).data(geo_data_store);
+        if (data === undefined) {
+            return geoData[name];
+        }
+        geoData[name] = data;
+    }
+};
 
 /**
  * Responds to a search request by displaying information on the map.
  * @param {object} search The search result object
  */
-function search_handler(search) {
-    console.log(search);
+function make_search_handler(node) {
+    var $el = $(node);
+    $el.geojs();
+
+    return function handler(search) {
+        $el.geojs('data', 'search', search);
+        console.log(search);
+    };
 }
 
-module.exports = {
-    handlers: {
-        search: search_handler
+/**
+ * Create a simple jquery plugin to initialize a map on a node.
+ * The `this` context should be the DOM element.
+ */
+$.fn.geojs = function (method) {
+    var $this = $(this),
+        geoData = $this.data(geo_data_store) || {},
+        map = geoData.map,
+        osm = geoData.osm,
+        layer = geoData.layer,
+        point = geoData.points;
+
+    // Initialize the map on first call
+    if (!map) {
+        map = geo.map({node: this});
+        geoData.map = map;
+        osm = null;
     }
+
+    if (!osm) {
+        osm = map.createLayer('osm');
+        geoData.osm = osm;
+        layer = null;
+    }
+
+    if (!layer) {
+        layer = map.createLayer('feature');
+        geoData.layer = layer;
+        point = null;
+    }
+
+    if (!point) {
+        point = layer.createFeature('point');
+        geoData.point = point;
+    }
+
+    if (geo_methods.hasOwnProperty(method)) {
+        geo_methods[method].apply(this, arguments.slice(1));
+    } else if (method) {
+        throw new Error('Unknown method "' + method + '"');
+    }
+
+    // Store the objects on the node
+    $this.data(geo_data_store, geoData);
+};
+
+module.exports = {
+    search: make_search_handler
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var webpack = require('webpack'),
+    BowerWebpackPlugin = require('bower-webpack-plugin');
+
 module.exports = {
     entry: './src/main.js',
     output: {
@@ -9,7 +12,29 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
             { test: /\.json?$/, loader: 'json' },
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' }
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' },
+            {
+                test: /geo.js$/,
+                loader: 'imports?this=>window'
+            }
         ]
-    }
+    },
+    resolve: {
+        alias: {
+            jquery: 'jquery/src/jquery',
+            geojs: 'geojs/geo.js',
+            proj4: 'proj4/lib/index.js'
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            mat4: 'gl-mat4',
+            vec2: 'gl-vec2',
+            vec3: 'gl-vec3',
+            vec4: 'gl-vec4',
+            proj4: 'proj4',
+            d3: 'd3'
+        })
+    ]
 };
